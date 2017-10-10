c2d56b9670b87f745fd99258ad632bb2b18f1216
tanhq_servlet@163.com <tanhq_servlet@163.com>
2017-10-10 20:06:31 +0800

    save todos

--- app/views.py
+++ app/views.py
@@ -1,8 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from app.models import Todo,Student
 
 @app.route('/')
 def index():
 	todos = Todo.objects.all()
 	return render_template("index.html",todos=todos)
+
+@app.route('/add',methods=['POST',])
+def add():
+	content = request.form.get("content")
+	errormsg = None
+	if content == "":
+		errormsg="please input content!"
+	else:
+		todo = Todo(content=content)
+		todo.save()
+	return redirect(url_for('index'))
