ce30dfa36333a8d7302d31d2772775f33c691e2c
tanhq_servlet@163.com <tanhq_servlet@163.com>
2017-10-11 19:34:38 +0800

    update save delete todo

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template,request,redirect,url_for
 from app.models import Todo,Student
+import datetime
 
 @app.route('/')
 def index():
@@ -16,4 +17,27 @@ def add():
 	else:
 		todo = Todo(content=content)
 		todo.save()
+	return redirect(url_for('index'))
+
+@app.route('/done/<string:todo_id>')
+def done(todo_id):
+	todo = Todo.objects.get_or_404(id=todo_id)
+	todo.status = 1
+	todo.time = datetime.datetime.now()
+	todo.save()
+	return redirect(url_for('index'))
+
+@app.route('/undone/<string:todo_id>')
+def undone(todo_id):
+	todo = Todo.objects.get_or_404(id=todo_id)
+	todo.status = 0
+	todo.time = datetime.datetime.now()
+	todo.save()
+	return redirect(url_for('index'))
+
+@app.route('/delete/<string:todo_id>')
+def delete(todo_id):
+	todo = Todo.objects.get_or_404(id=todo_id)
+	todo.delete()
+	todo.save()
 	return redirect(url_for('index'))
